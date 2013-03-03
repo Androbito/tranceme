7690233049c6f8a6f29cc0c1ac643cb032f8cbda
naciri.ay@gmail.com <naciri.ay@gmail.com@7b3288c9-bbec-aa82-0d82-25acaa35b22b>
2013-03-03 13:43:51 +0000

    recupération de list des hits

--- trunk/TranceMe/src/com/gnb/coverflow/ImageAdapter.java
+++ trunk/TranceMe/src/com/gnb/coverflow/ImageAdapter.java
@@ -27,84 +27,84 @@ public class ImageAdapter extends BaseAdapter {
 
 	private FileInputStream fis;
 
-	private Integer[] mImageIds = { R.drawable.kasabian_kasabian,
-			R.drawable.starssailor_silence_is_easy,
-			R.drawable.killers_day_and_age, R.drawable.garbage_bleed_like_me,
-			R.drawable.death_cub_for_cutie_the_photo_album,
-			R.drawable.kasabian_kasabian, R.drawable.massive_attack_collected,
-			R.drawable.muse_the_resistance,
-			R.drawable.starssailor_silence_is_easy };
+//	private Integer[] mImageIds = { R.drawable.kasabian_kasabian,
+//			R.drawable.starssailor_silence_is_easy,
+//			R.drawable.killers_day_and_age, R.drawable.garbage_bleed_like_me,
+//			R.drawable.death_cub_for_cutie_the_photo_album,
+//			R.drawable.kasabian_kasabian, R.drawable.massive_attack_collected,
+//			R.drawable.muse_the_resistance,
+//			R.drawable.starssailor_silence_is_easy };
 
 	private ImageView[] mImages;
 
-	public ImageAdapter(Context c) {
+	public ImageAdapter(Context c,ImageView[] images) {
 		mContext = c;
-		mImages = new ImageView[mImageIds.length];
+		mImages = images;
 	}
 
-	public boolean createReflectedImages() {
-		// The gap we want between the reflection and the original image
-		final int reflectionGap = 4;
-
-		int index = 0;
-		for (int imageId : mImageIds) {
-			Bitmap originalImage = BitmapFactory.decodeResource(
-					mContext.getResources(), imageId);
-			int width = originalImage.getWidth();
-			int height = originalImage.getHeight();
-
-			// This will not scale but will flip on the Y axis
-			Matrix matrix = new Matrix();
-			matrix.preScale(1, -1);
-
-			// Create a Bitmap with the flip matrix applied to it.
-			// We only want the bottom half of the image
-			Bitmap reflectionImage = Bitmap.createBitmap(originalImage, 0,
-					height / 2, width, height / 2, matrix, false);
-
-			// Create a new bitmap with same width but taller to fit reflection
-			Bitmap bitmapWithReflection = Bitmap.createBitmap(width,
-					(height + height / 2), Config.ARGB_8888);
-
-			// Create a new Canvas with the bitmap that's big enough for
-			// the image plus gap plus reflection
-			Canvas canvas = new Canvas(bitmapWithReflection);
-			// Draw in the original image
-			canvas.drawBitmap(originalImage, 0, 0, null);
-			// Draw in the gap
-			Paint deafaultPaint = new Paint();
-			canvas.drawRect(0, height, width, height + reflectionGap,
-					deafaultPaint);
-			// Draw in the reflection
-			canvas.drawBitmap(reflectionImage, 0, height + reflectionGap, null);
-
-			// Create a shader that is a linear gradient that covers the
-			// reflection
-			Paint paint = new Paint();
-			LinearGradient shader = new LinearGradient(0,
-					originalImage.getHeight(), 0,
-					bitmapWithReflection.getHeight() + reflectionGap,
-					0x70ffffff, 0x00ffffff, TileMode.CLAMP);
-			// Set the paint to use this shader (linear gradient)
-			paint.setShader(shader);
-			// Set the Transfer mode to be porter duff and destination in
-			paint.setXfermode(new PorterDuffXfermode(Mode.DST_IN));
-			// Draw a rectangle using the paint with our linear gradient
-			canvas.drawRect(0, height, width, bitmapWithReflection.getHeight()
-					+ reflectionGap, paint);
-
-			ImageView imageView = new ImageView(mContext);
-			imageView.setImageBitmap(bitmapWithReflection);
-			imageView.setLayoutParams(new CoverFlow.LayoutParams(120, 180));
-			imageView.setScaleType(ScaleType.MATRIX);
-			mImages[index++] = imageView;
-
-		}
-		return true;
-	}
+//	public boolean createReflectedImages() {
+//		// The gap we want between the reflection and the original image
+//		final int reflectionGap = 4;
+//
+//		int index = 0;
+//		for (int imageId : mImageIds) {
+//			Bitmap originalImage = BitmapFactory.decodeResource(
+//					mContext.getResources(), imageId);
+//			int width = originalImage.getWidth();
+//			int height = originalImage.getHeight();
+//
+//			// This will not scale but will flip on the Y axis
+//			Matrix matrix = new Matrix();
+//			matrix.preScale(1, -1);
+//
+//			// Create a Bitmap with the flip matrix applied to it.
+//			// We only want the bottom half of the image
+//			Bitmap reflectionImage = Bitmap.createBitmap(originalImage, 0,
+//					height / 2, width, height / 2, matrix, false);
+//
+//			// Create a new bitmap with same width but taller to fit reflection
+//			Bitmap bitmapWithReflection = Bitmap.createBitmap(width,
+//					(height + height / 2), Config.ARGB_8888);
+//
+//			// Create a new Canvas with the bitmap that's big enough for
+//			// the image plus gap plus reflection
+//			Canvas canvas = new Canvas(bitmapWithReflection);
+//			// Draw in the original image
+//			canvas.drawBitmap(originalImage, 0, 0, null);
+//			// Draw in the gap
+//			Paint deafaultPaint = new Paint();
+//			canvas.drawRect(0, height, width, height + reflectionGap,
+//					deafaultPaint);
+//			// Draw in the reflection
+//			canvas.drawBitmap(reflectionImage, 0, height + reflectionGap, null);
+//
+//			// Create a shader that is a linear gradient that covers the
+//			// reflection
+//			Paint paint = new Paint();
+//			LinearGradient shader = new LinearGradient(0,
+//					originalImage.getHeight(), 0,
+//					bitmapWithReflection.getHeight() + reflectionGap,
+//					0x70ffffff, 0x00ffffff, TileMode.CLAMP);
+//			// Set the paint to use this shader (linear gradient)
+//			paint.setShader(shader);
+//			// Set the Transfer mode to be porter duff and destination in
+//			paint.setXfermode(new PorterDuffXfermode(Mode.DST_IN));
+//			// Draw a rectangle using the paint with our linear gradient
+//			canvas.drawRect(0, height, width, bitmapWithReflection.getHeight()
+//					+ reflectionGap, paint);
+//
+//			ImageView imageView = new ImageView(mContext);
+//			imageView.setImageBitmap(bitmapWithReflection);
+//			imageView.setLayoutParams(new CoverFlow.LayoutParams(120, 180));
+//			imageView.setScaleType(ScaleType.MATRIX);
+//			mImages[index++] = imageView;
+//
+//		}
+//		return true;
+//	}
 
 	public int getCount() {
-		return mImageIds.length;
+		return mImages.length;
 	}
 
 	public Object getItem(int position) {
